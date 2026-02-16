82afb3c3325c7fe99754aa169e203811c8765017
paul calver <thecalver@gmail.com>
2026-02-16 13:14:45 +0000

    YEOFR 0.3.94 – update URL + checksum

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "YEOFR",
-      url: "https://github.com/YEOMessaging/YEOFR-SPM/releases/download/0.3.93/YEOFR.xcframework.zip",
-      checksum: "95b0b94ea2a6930976a905d657c6b4bb0b95bbce74200244bef41b66840f83fe"
+      url: "https://github.com/YEOMessaging/YEOFR-SPM/releases/download/0.3.94/YEOFR.xcframework.zip",
+      checksum: "8732d374ef2b8447a53e956cc41d7a1451b11ad17f31e69662803c111bc3bd5a"
     )
   ]
 )
